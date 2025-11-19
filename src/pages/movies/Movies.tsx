94a969e638cecef8a6c81de33fe658e3d657b4a0
Irfan Fernando <irfan.fernando@yahoo.com>
2025-11-19 19:18:16 +0700

    First commit

---
+++ src/pages/movies/Movies.tsx
@@ -0,0 +1,41 @@
+import { useCallback, useEffect, useState } from "react"
+import { NavLink } from "react-router"
+import ApiClient from "../../utils/ApiClient";
+
+interface Movie {
+    _id: string;
+    judul: string;
+    tahunRilis: string;
+    sutradara: string;
+    createdAt: string;
+    updatedAt: string;
+
+}
+
+function Movies(){
+    const[movies, setMovies] = useState<Movie[]> ([]);
+
+    const fetchMovies = useCallback(async() => {
+        const response = await ApiClient.get("/movie")
+
+        if(response.status == 200){
+            setMovies(response.data.data)
+        }
+
+    }, []);
+
+    useEffect(() =>{
+        fetchMovies()
+    },[fetchMovies])
+
+
+
+    return <div className="container mx-auto">
+        <div className="d-flex justify-content-between mb-1">
+            <h2>Movie Page</h2>
+            <NavLink to="/add-movie"className="btn btn-primary">AddMovie</NavLink>
+        </div>
+    </div>
+}
+
+export default Movies
---
+++ src/routes/router.ts
@@ -0,0 +1,29 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        children: [
+            {
+                index:true,
+                lazy:{
+                    Component : async () => {
+                        const component = await import("../pages/movies/Movies.tsx")
+                        return component.default
+                    }
+                }
+            },
+            {
+                path:"add-movie",
+                lazy:{
+                    Component : async () => {
+                        const Component = await import("../pages/movies/AddMovie.tsx")
+                        return Component.default
+                    }
+                }
+            }
+        ]
+    }
+])
+
+export default router
